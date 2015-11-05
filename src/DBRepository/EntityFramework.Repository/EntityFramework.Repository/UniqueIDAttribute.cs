2599bc1c76f698bfed64a811a8bca0978dd11f96
liangyi <liangyi@mallcoo.cn>
2015-11-05 16:17:55 +0800

    1

---
+++ src/DBRepository/EntityFramework.Repository/EntityFramework.Repository/UniqueIDAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Repository
+{
+    /// <summary>
+    /// 跟时间顺序相关的唯一值
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class UniqueIDAttribute : Attribute
+    {
+    }
+}
